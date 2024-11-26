44a560c4725b47788c6e8754772f44fd6b166343
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-25 22:46:17 -0300

    conctact route, not found implemented

--- http.js
+++ http.js
@@ -3,10 +3,16 @@ const http = require('node:http')
 const desiredPort = process.env.PORT ?? 3000
 
 const processRequest = (req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
     if (req.url === '/'){
         res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('Bienvenido a la pagina de inicio')
+        res.end('<h1>Bienvenido a la pagina de inicio</h1>')
+    } else if (req.url === '/contacto'){
+        res.statusCode = 200
+        res.end('<h1>Bienvenido a la pagina de contacto</h1>')
+    } else {
+        res.statusCode = 404
+        res.end('<h1>404 Not Found</h1>')
     }
 }
 
