214aa4fe26b523e8101cd10503fb0a050f2c4b0c
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-26 11:30:07 -0300

    Commented middleware does the same as native function express.json

--- express.js
+++ express.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 const ditto = require('./pokemon/ditto.json')
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   if (req.method !== 'POST') {
     return next()
   }
@@ -21,7 +21,9 @@ app.use((req, res, next) => {
     req.body = data
     next()
   })
-})
+}) */
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Mi pagina</h1>')
@@ -35,7 +37,7 @@ app.post('/pokemon', (req, res) => {
   res.status(201).json(req.body)
 })
 
-app.use((res) => {
+app.use((req, res) => {
   res.status(404).send('<h1>404 Not Found</h1>')
 })
 
