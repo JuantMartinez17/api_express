b08d37479573aa6b2cae8c2d85b28584c6114776
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-26 11:27:36 -0300

    Primer middleware creado

--- express.js
+++ express.js
@@ -3,6 +3,26 @@ const app = express()
 const PORT = process.env.PORT ?? 3000
 const ditto = require('./pokemon/ditto.json')
 
+app.use((req, res, next) => {
+  if (req.method !== 'POST') {
+    return next()
+  }
+  if (req.headers['content-type'] !== 'application/json') {
+    return next()
+  }
+  // A este punto solo llegan las request que sean de tipo post y content type application/json
+  let body = ''
+  req.on('data', (chunk) => {
+    body += chunk.toString()
+  })
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    req.body = data
+    next()
+  })
+})
+
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Mi pagina</h1>')
 })
@@ -12,14 +32,7 @@ app.get('/pokemon/ditto', (req, res) => {
 })
 
 app.post('/pokemon', (req, res) => {
-  let body = ''
-  req.on('data', (chunk) => {
-    body += chunk.toString()
-  })
-  req.on('end', () => {
-    const data = JSON.parse(body)
-    res.status(201).json(data)
-  })
+  res.status(201).json(req.body)
 })
 
 app.use((res) => {
