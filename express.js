f6aadb8375ac70fe721da9419a5496028a6ec7b1
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-26 00:47:02 -0300

    Migration completed

--- express.js
+++ express.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT ?? 3000
+const ditto = require('./pokemon/ditto.json')
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Mi pagina</h1>')
+})
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
 })
 
 app.post('/pokemon', (req, res) => {
@@ -15,6 +20,10 @@ app.post('/pokemon', (req, res) => {
     const data = JSON.parse(body)
     res.status(201).json(data)
   })
+})
+
+app.use((res) => {
+  res.status(404).send('<h1>404 Not Found</h1>')
 })
 
 app.listen(PORT, () => {
