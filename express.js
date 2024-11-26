7147244e7076ec647cf1855e4f49e2bb5d412bdb
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-26 00:41:44 -0300

    POST with express

--- express.js
+++ express.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Mi pagina</h1>')
 })
 
+app.post('/pokemon', (req, res) => {
+  let body = ''
+  req.on('data', (chunk) => {
+    body += chunk.toString()
+  })
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    res.status(201).json(data)
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
