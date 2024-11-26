ff425aa229c40581ab7aac0660ba14820cc920b6
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-25 23:06:50 -0300

    static resource route

--- http.js
+++ http.js
@@ -1,23 +1,36 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 3000
 
 const processRequest = (req, res) => {
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    if (req.url === '/'){
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.end('<h1>Bienvenido a la pagina de inicio</h1>')
+  } else if (req.url === '/cat.png') {
+    res.statusCode = 200
+    fs.readFile('./imagen.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>Internal Server Error</h1>')
+      } else {
         res.statusCode = 200
-        res.end('<h1>Bienvenido a la pagina de inicio</h1>')
-    } else if (req.url === '/contacto'){
-        res.statusCode = 200
-        res.end('<h1>Bienvenido a la pagina de contacto</h1>')
-    } else {
-        res.statusCode = 404
-        res.end('<h1>404 Not Found</h1>')
-    }
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200
+    res.end('<h1>Bienvenido a la pagina de contacto</h1>')
+  } else {
+    res.statusCode = 404
+    res.end('<h1>404 Not Found</h1>')
+  }
 }
 
 const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
-    console.log(`Server listening on port http://localhost:${desiredPort}`)
+  console.log(`Server listening on port http://localhost:${desiredPort}`)
 })
