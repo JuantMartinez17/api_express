c36b7b6baa0fcf16acc989e4290db0de058221db
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-25 22:40:43 -0300

    process request function created routes separation implemented

--- http.js
+++ http.js
@@ -2,10 +2,15 @@ const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-  console.log('request received', req.url)
-  res.end('Hola mundo')
-})
+const processRequest = (req, res) => {
+    if (req.url === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/plain')
+        res.end('Bienvenido a la pagina de inicio')
+    }
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
     console.log(`Server listening on port http://localhost:${desiredPort}`)
